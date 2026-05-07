07a72a0d91509b9d77824ace312ec61992142077
acaiamity <acaiamity.repo@gmail.com>
2026-05-07 17:29:39 +0530

    Added header and logos

--- app.py
+++ app.py
@@ -124,6 +124,12 @@ def display_permanent_logo():
 
 display_permanent_logo()
 
+col1, col2, col3 = st.columns([3, 4, 1])
+with col2:
+    st.image("amity_logo.png")
+
+
+
 # Custom Medical Theme CSS
 st.markdown("""
 <style>
